710696ebb134e1d6b688059b19dce19b19e8fb11
Pakonieczny <pakonieczny@gmail.com>
2026-03-30 13:35:30 -0400

    Add files via upload

--- cherry-viewer/vite.config.js
+++ cherry-viewer/vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: 'cherry-viewer',
+  base: '/cherry-viewer/',
   build: {
-    outDir: '../dist/cherry-viewer',
+    outDir: '../cherry-viewer-dist',
     emptyOutDir: true,
   },
 });
